1eae00f0569ba2b66220485aa56dd8ee62682916
Robert <robert@noemail.com>
2025-07-14 11:47:43 +0200

    sketching to add note triggers

--- synth_one.c
+++ synth_one.c
@@ -17,10 +17,48 @@ static size_t calc_frame_size(const SDL_AudioSpec *spec) {
   return spec->channels * SDL_AUDIO_BYTESIZE(spec->format);
 }
 
+struct trigger {
+	char note;
+	float freq;
+	long long start_frame;
+	long long stop_frame; // 0 if not set yet
+};
+
+#define MAX_TRIGGERS (1)
+
+struct trigger triggers[MAX_TRIGGERS] = {};
+
+
+static void create_note(char note, long long current_frame);
+{
+	int i;
+	for (i = 0; i < MAX_TRIGGERS; i++)
+	{
+		if (stop_frame && stop_frame <= current_frame) {
+			triggers[i].note = c;
+			triggers[i].start_frame = current_frame;
+			triggers[i].stop_frame = current_frame + 410;
+			triggers[i].freq = note_to_freq(c);
+		}
+
+	}
+
+}
+
+/*
+ * width - [0.0 1.0]
+ * freq - integer Hz
+ * current_frame - incremental frame number */
 static float render_pulse(const long long current_frame,
                           const SDL_AudioSpec *spec, float freq, float width) {
   int frames_per_period = spec->freq / freq;
   int frames_past_last_period = current_frame % frames_per_period;
+
+  if (width > 1.0)
+    width = 1.0;
+  if (width < 0.0)
+    width = 0.0;
+
   if (frames_past_last_period > (int)(width * frames_per_period))
     return -1.0;
   else
@@ -29,12 +67,12 @@ static float render_pulse(const long long current_frame,
 static float render_sine(const long long current_frame,
                          const SDL_AudioSpec *spec, float freq) {
   float time = current_frame * 1.0 / spec->freq;
-  return 1.0-0.4*(1.0+cos(freq*2 * M_PI * time));
+  return 1.0 - 0.4 * (1.0 + cos(freq * 2 * M_PI * time));
 }
 
 static float render_sample(const long long current_frame,
                            const SDL_AudioSpec *spec) {
-  float width = 0.5*render_sine(current_frame, spec, 0.3);
+  float width = 0.5 * render_sine(current_frame, spec, 0.3);
   return render_pulse(current_frame, spec, 150, width);
 }
 
