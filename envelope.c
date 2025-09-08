310dd8113338e2b1d8278faa069a9d3dab2fa6cf
Robert <robert@noemail.com>
2025-09-08 20:51:19 +0200

    Relase from latest env val and fix int div

--- envelope.c
+++ envelope.c
@@ -1,4 +1,5 @@
 #include "envelope.h"
+#include <stdio.h>
 
 #define min(x, y) ((x) < (y) ? x : y)
 #define max(x, y) ((x) < (y) ? y : x)
@@ -9,6 +10,7 @@ long long release_frame = -44200;
 int attack_frames = 10;
 int decay_frames = 10;
 float sustain_value = 0.7;
+float release_level = 0.5;
 int release_frames = 1;
 
 static float ms_to_frames(SDL_AudioSpec *spec, float ms)
@@ -36,6 +38,7 @@ void envelope_release(long long frame)
 
 float envelope_get(long long frame)
 {
+    float ret_level;
     if (frame < start_frame)
     {
         printf("Bad startframe %lld or frame %lld\n", start_frame, frame);
@@ -48,20 +51,26 @@ float envelope_get(long long frame)
         long long sustain_start = start_frame + attack_frames + decay_frames;
         if (frame > sustain_start)
         {
-            return sustain_value;
+            ret_level = sustain_value;
         }
         else if (frame > decay_start)
         {
             // decay
-            return (1.0 - ((1.0 - sustain_value) * (frame - decay_start)) / decay_frames);
+            ret_level = (1.0 - ((1.0 - sustain_value) * (frame - decay_start)) / decay_frames);
         }
-        // attack
-        return (1.0 * (frame - start_frame)) / attack_frames;
+        else
+        {
+            // attack
+            ret_level = (1.0 * (frame - start_frame)) / attack_frames;
+        }
     }
     else
     {
         // Release
         int diff = frame - release_frame;
-        return max(0.0, sustain_value * (1.0 - diff / (release_frames)));
+        return max(0.0, release_level * (1.0 - 1.0 * diff / (release_frames)));
     }
+
+    release_level = ret_level;
+    return ret_level;
 }
